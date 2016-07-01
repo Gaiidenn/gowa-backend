d9634598ad83811ead2d5ce3ef0e6da27ac1e733
Jerome <jerome@gaatech.be>
2016-07-01 10:44:25 +0200

    now client need a token generated by server for ws connection

--- rpcWebsocket/hub.go
+++ rpcWebsocket/hub.go
@@ -3,6 +3,7 @@ package rpcWebsocket
 import (
 	"log"
 	"fmt"
+	"math/rand"
 )
 
 // hub maintains the set of active connections and broadcasts messages to the
@@ -18,14 +19,16 @@ type hub struct {
 	unregister chan *connection
 
 	// Registered connections.
-	connections map[*connection]bool
+	connections map[string]*connection
 }
+
+const KEY_LENGTH = 8
 
 var h = hub{
 	broadcast:   make(chan *RpcCall),
 	register:    make(chan *connection),
 	unregister:  make(chan *connection),
-	connections: make(map[*connection]bool),
+	connections: make(map[string]*connection),
 }
 
 func RunHub() {
@@ -40,25 +43,81 @@ func (h *hub) run() {
 	for {
 		select {
 		case c := <-h.register:
-			h.connections[c] = true
+			log.Println("trying to register")
+			key := h.generateKey(c)
+			h.connections[key] = c
+			log.Println("Connection registered! key: ", key)
+			var reply bool
+			call := &RpcCall{
+				Method: "App.setToken",
+				Args: key,
+				Reply: &reply,
+			}
 			fmt.Println("Number of connections : ")
 			fmt.Println(len(h.connections))
+			c.call <- call
 		case c := <-h.unregister:
-			if _, ok := h.connections[c]; ok {
-				delete(h.connections, c)
-				close(c.call)
+			for key, cTmp := range h.connections {
+				if cTmp == c {
+					delete(h.connections, key)
+					close(c.call)
+				}
 			}
 		case m := <-h.broadcast:
 			log.Println("trying to broadcast")
 			log.Println(m)
-			for c := range h.connections {
+			for key, c := range h.connections {
 				select {
 				case c.call <- m:
 				default:
 					close(c.call)
-					delete(h.connections, c)
+					delete(h.connections, key)
 				}
 			}
 		}
 	}
+}
+
+const lettersBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func (h *hub) generateKey(c *connection) string {
+	key := randStringBytes(KEY_LENGTH)
+	log.Println("First key : ", key)
+	v, ok := h.connections[key]
+	for ok == true || (v != nil && v != c) {
+		key = randStringBytes(KEY_LENGTH)
+		v, ok = h.connections[key];
+		log.Println("Other key : ", key)
+	}
+	log.Println("v, ok : ", v, ok)
+	return key
+}
+
+func cleanKey(key []byte) string {
+	var skey = ""
+	for _, v := range key {
+		valid := false
+		for _, lchar := range lettersBytes {
+			if string(v) == string(lchar) {
+				valid = true
+			}
+		}
+		if valid {
+			skey = skey + string(v)
+		}
+	}
+	return skey
+}
+
+func (h *hub) keyExists(key string) bool {
+	_, ok := h.connections[key];
+	return ok
+}
+
+func randStringBytes(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = lettersBytes[rand.Intn(len(lettersBytes))]
+	}
+	return string(b)
 }
--- rpcWebsocket/websocket.go
+++ rpcWebsocket/websocket.go
@@ -1,15 +1,11 @@
 package rpcWebsocket
 
 import (
-	//"log"
-	//"net/http"
 	"golang.org/x/net/websocket"
 	"net/rpc"
 	"net/rpc/jsonrpc"
 	"log"
-	//"time"
 	"time"
-	"fmt"
 )
 
 // connection is an middleman between the websocket connection and the hub.
@@ -32,6 +28,17 @@ type RpcCall struct {
 
 func JsonrpcHandler(ws *websocket.Conn) {
 	log.Println("connection websocket on jsonrpcHandler")
+	var key = make([]byte, KEY_LENGTH + 4) // KEY_LENGTH + 4 because what we receive is ["..key."]
+	_, err := ws.Read(key)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	skey := cleanKey(key)
+	if !h.keyExists(skey) {
+		log.Println("invalid key")
+		return
+	}
 	jsonrpc.ServeConn(ws)
 }
 
@@ -47,15 +54,6 @@ func PushHandler(ws *websocket.Conn) {
 	}
 	h.register <- c
 	c.callPump()
-	/*
-	var reply string
-
-	err := rc.Call("App.log", "My test", &reply)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	log.Println(reply)*/
 }
 
 // callPump pumps calls from the hub to the rpc connection.
@@ -82,10 +80,8 @@ func (c *connection) callPump() {
 			log.Println(call.Reply)
 		case <- ticker.C :
 			if _, err := c.ws.Write([]byte{}); err != nil {
-				fmt.Println("no response from client -> closing connection!")
 				return
 			}
-			fmt.Println("client still connected -> ok!")
 		}
 	}
 }
